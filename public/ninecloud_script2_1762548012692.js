4f7a14c53411520893c69fe169ebbabea54e6e92
Nine12 <banianjay004@gmail.com>
2025-11-08 03:40:14 +0700

    Upload ninecloud_script2_1762548012692.js

---
+++ public/ninecloud_script2_1762548012692.js
@@ -0,0 +1,5 @@
+// Ini contoh aja, misalnya pas video diputar kita log sesuatu
+const video = document.getElementById('myVideo');
+video.addEventListener('play', () => {
+    console.log('Video sedang diputar...');
+});
